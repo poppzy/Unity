d077e73f32ebf65d2c0fc29ac1fc819e6991cf3b
Oscar Wilhelmsson <karloscarwilhelmsson@gmail.com>
2020-10-14 11:16:51 +0200

    Added "Roll-a-ball tutorial" assignment

---
+++ MovementExample/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rb2d;
+    Vector2 movement = new Vector2();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        movement = new Vector2(x, y);
+
+        if (movement.sqrMagnitude > 1)
+        {
+            movement = movement.normalized;
+        }
+    }
+
+    //Update is called every physics interval (0.02s) before physics (can be adjusted)
+    private void FixedUpdate()
+    {
+        rb2d.velocity = movement * speed;
+    }
+}
---
+++ Roll-a-ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Rotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
