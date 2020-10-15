ab997750097a2297b81058a4dc687697b0d56320
Oscar Wilhelmsson <karloscarwilhelmsson@gmail.com>
2020-10-15 10:29:38 +0200

    Added "MovementExample": 12 - Input and Movement (in Unity)

---
+++ MovementExample/Assets/PlayerControllerRED.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerRED : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, y, 0) * speed * Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
